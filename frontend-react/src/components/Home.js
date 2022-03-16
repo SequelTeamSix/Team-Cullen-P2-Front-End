1d9273a49813de9c336eb1b2812502c2015cd454
Cullen <cullenkuch@gmail.com>
2022-03-16 17:47:33 -0400

    home page strucutre

--- frontend-react/src/components/Home.js
+++ frontend-react/src/components/Home.js
@@ -1,18 +1,56 @@
 import React from "react";
+import { useState } from 'react';
 import '../css/Home.css';
 import { Link } from 'react-router-dom';
 
 
 function Home() {
+
+    const [rulesDisplay, setRulesDisplay] = useState('none');
+
+    function openRulesPopup(){
+        setRulesDisplay('block')
+      }
+
+      function closeRulesPopup(){
+        setRulesDisplay('none')
+      }
+
+
     return (
     <div>
-        <div className="home-div">
-            <h1>Inside Home Page</h1>
-            <Link to="/">Go To Login</Link>
-            <Link to="/game">Go To Game</Link>
-            <Link to="/store">Go To Store</Link>
-            <Link to="/deckbuilder">Go To Deck Builder</Link>
-      </div>
+        <div className="main-page">
+        <p className="current-user">Welcome, Username</p>
+        <Link to="/">Go To Login</Link>
+            <div className="container">
+                <h3 className="main-title">Superhero Card Duel</h3>
+                <div className="menu">
+                    <Link className="menu-link" to="/game">Play Game</Link>
+                    <Link className="menu-link" to="/store">Visit Store</Link>
+                    <Link className="menu-link" to="/deckbuilder">My Deck Builder</Link>
+                    <p className="menu-link" onClick={openRulesPopup}>Read Rules</p>
+                </div>
+
+                <div className="rules-outer-div" style={{display: rulesDisplay}}>
+                    <div className="rules-inner-div">
+                        <span className="close-btn" onClick={closeRulesPopup}>X</span>
+                        <p className="rules-paragraph">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="leaderboard-div">
+                    <h3>Leaderboard</h3>
+                </div>
+            </div>
+        </div>
     </div>
     
     );
--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -15,12 +15,15 @@ import { Link } from 'react-router-dom';
       setPopupDisplay('none')
     }
 
+    function confirmNewAccount(){
+
+    }
+
     return (
       <div>
-        <h1>Inside Login Page</h1>
         <Link to="/home">Go To Home</Link>
         <div className="container">
-          <form className="login-form" action="action_page.php" method="post">
+          <form className="login-form" action="" method="post">
               <div className="form-field-div">
                 <label htmlFor="username">Username</label>
                 <input type="text" placeholder="Enter Username" name="username"/>
@@ -39,6 +42,24 @@ import { Link } from 'react-router-dom';
             <div className="new-account-content">
               <h2>Create New Account</h2>
               <span className="close-btn" onClick={closeAccountDiv}>X</span>
+              <form className="new-account-form" action="" method="post">
+                <div className="form-field-div">
+                  <label htmlFor="username">Username</label>
+                  <input type="text" placeholder="Enter Username" name="username"/>
+                </div>
+                
+                <div className="form-field-div">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" placeholder="Enter Password" name="password" />
+                </div>
+
+                <div className="form-field-div">
+                  <label htmlFor="password-confirm">Confirm Password</label>
+                  <input type="password" placeholder="Confirm Your Password" name="password-confirm" />
+                </div>
+
+                <button id="confirm-account-btn" type="submit" onClick={confirmNewAccount}>Create New Account</button>
+              </form>
             </div>
            
 
