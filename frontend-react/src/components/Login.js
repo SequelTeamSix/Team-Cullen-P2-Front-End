f94181278811797615a1b2108e4b53035eb18406
Cullen <cullenkuch@gmail.com>
2022-03-18 13:07:58 -0400

    made login component a class

--- frontend-react/src/components/Login.js
+++ frontend-react/src/components/Login.js
@@ -1,34 +1,44 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import { useState, useEffect } from 'react';
 import '../css/Login.css';
 import { Link } from 'react-router-dom';
+// import { render } from "react-dom";
 
 
-  export default function Login() {
+ export default class Login extends React.Component {
 
-    const [players, setPlayers] = useState(null);
-    const [popupDisplay, setPopupDisplay] = useState('none');
-    
+  constructor(props){
+    super(props);
+    this.state = {
+     allPlayers: [],
+     newUserPopupDisplay: 'none',
+     currentUser: []
+    }
+  }
 
-    //Get All The Saved Players 
-    useEffect(() => {
-      fetch('http://localhost:8000/player')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setPlayers(data)
-        })
-    }, []);
-    
-    function ValidateLogin(e){
-      
+  //Get all the saved players from the database and store them
+  componentDidMount(){
+    this.getAllUsers();
+  }
+  
+  getAllUsers(){
+    axios.get('http://localhost:8000/player')
+    .then(response => {
+      let returnedPlayers = response.data;
+      this.setState({allPlayers : returnedPlayers})
+    })
+  }
+   
+    //Logging in to a saved account
+    validateLogin(e){
       e.preventDefault();
       let providedUsername = document.getElementById("login-username").value;
       let providedPassword = document.getElementById("login-password").value;
       let homeBtn = document.getElementById('home-btn');
-      
-      let matchingUser = players.filter(player => player.username === providedUsername);
+      console.log(this.state.allPlayers)
+      let matchingUser = this.state.allPlayers.filter(player => player.username === providedUsername);
+      console.log(matchingUser)
       if (matchingUser.length === 0){
         alert('Incorrect Username/Password')
         return;
@@ -38,24 +48,53 @@ import { Link } from 'react-router-dom';
       } 
     }
 
-    function openNewAccountDiv(){
-      setPopupDisplay('block')
+    //Creating a new account
+    confirmNewAccount(e){
+      e.preventDefault();
+      let newUsername = document.getElementById('new-account-username');
+      let newPassword1 = document.getElementById('new-account-password1');
+      let newPassword2 = document.getElementById('new-account-password2');
+
+      if(newPassword1 !== newPassword2){
+        alert("Oops, your passwords don't match")
+        return;
+      }
+      this.AddNewPlayerToDatabase(newUsername, newPassword1);
     }
 
-    function closeAccountDiv(){
-      setPopupDisplay('none')
-    }
+    //Add new player to the database 
+      AddNewPlayerToDatabase(username, password){
+        const newUser = {
+          'username': username,
+          'password': password,
+          'points': 0,
+          'wins': 0,
+          'loses': 0
+        }
 
-    function confirmNewAccount(){
-
-    }
+        axios.put('http://localhost:8000/player/add', newUser)
+          .then(response => {
+            let newPlayer = response.data;
+            this.setState({currentUser : newPlayer})
+          })
+          
+      }
     
+      //Popup Menu Hiding and Displaying
+      openNewAccountDiv(){
+       this.setState({ newUserPopupDisplay : 'block'})
+      }
+  
+      closeNewAccountDiv(){
+        this.setState({ newUserPopupDisplay : 'none'})
+      }
 
-    return (    
-      <div>
+    render(){
+      return (
+       <div>
         <Link id="home-btn" to="/home">Go To Home</Link>
         <div className="container">
-          <form className="login-form" onSubmit={ValidateLogin} method="post">
+          <form className="login-form" onSubmit={(e) => this.validateLogin(e)} method="post">
               <div className="form-field-div">
                 <label htmlFor="username">Username</label>
                 <input id="login-username" type="text" placeholder="Enter Username" name="username" required/>
@@ -67,37 +106,36 @@ import { Link } from 'react-router-dom';
               </div>
             
               <button type="submit">Login</button>
-              <button id="new-account-btn" type="button" onClick={openNewAccountDiv}>Create New Account</button>
+              <button id="new-account-btn" type="button" onClick={() => this.openNewAccountDiv()}>Create New Account</button>
           </form>
 
-            <div id="new-account-div" style={{display: popupDisplay}}>
+            <div id="new-account-div" style={{display: this.state.newUserPopupDisplay}}>
               <div className="new-account-content">
                 <h2>Create New Account</h2>
-                <span className="close-btn" onClick={closeAccountDiv}>X</span>
+                <span className="close-btn" onClick={() => this.closeNewAccountDiv()}>X</span>
                 <form className="new-account-form" action="" method="post">
                   <div className="form-field-div">
                     <label htmlFor="username">Username</label>
-                    <input type="text" placeholder="Enter Username" name="username"/>
+                    <input id="new-account-username" type="text" placeholder="Enter Username" name="username"/>
                   </div>
                   
                   <div className="form-field-div">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter Password" name="password" />
+                    <input id="new-account-password1" type="password" placeholder="Enter Password" name="password" />
                   </div>
 
                   <div className="form-field-div">
                     <label htmlFor="password-confirm">Confirm Password</label>
-                    <input type="password" placeholder="Confirm Your Password" name="password-confirm" />
+                    <input id="new-account-password2" type="password" placeholder="Confirm Your Password" name="password-confirm" />
                   </div>
 
-                  <button id="confirm-account-btn" type="submit" onClick={confirmNewAccount}>Create New Account</button>
+                  <button id="confirm-account-btn" type="submit" onClick={(e) => this.confirmNewAccount(e)}>Create New Account</button>
+
                 </form>
               </div>
             </div>
         </div>
       </div>
-    );
+    )
   }
-
-
-  
+}
